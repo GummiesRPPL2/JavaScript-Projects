e75c462a08501a9d38c2631053e1099af5ee89cc
GummiesRPPL2 <137831177+GummiesRPPL2@users.noreply.github.com>
2023-09-22 12:44:03 -0500

    started a basic javascript project folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+//This will make a popup that states the greeting//
+var greeting = "Hello, world!"
+window.alert(greeting)
+
+//Assigning variables//
+var sent1 = "This is the beginning of the string"
+var sent2 = " and this is the end of the string."
+
+//Concatenating 2 strings//
+document.write(sent1 + sent2)
+
+document.write(56+34)
+
