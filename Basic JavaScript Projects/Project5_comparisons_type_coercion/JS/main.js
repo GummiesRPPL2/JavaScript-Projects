7edce71a0c2d834f46059eaf85103ab52dfd977e
GummiesRPPL2 <137831177+GummiesRPPL2@users.noreply.github.com>
2023-09-26 15:28:09 -0500

    Comparison assignment

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,39 @@
+document.write(typeof("Hello, world!"))
+
+document.write("Hi there" + 16)
+
+document.write(4.2E459)
+
+document.write(-5.6E402)
+
+document.write(6 > 7)
+
+document.write(8 < 15)
+
+console.log(16 * 4)
+
+console.log(15 > 100)
+
+document.write(16 == 14)
+
+document.write(15 == 15)
+
+document.write(15 === 15)
+
+document.write(15 === "15")
+
+document.write("16" === 15)
+
+document.write(21 === 32)
+
+document.write(16 > 12 && 8 < 14)
+
+document.write(15 > 45 && 15 > 12)
+
+document.write(35 > 15 || 24 < 12)
+
+document.write(23 < 12 || 45 < 23)
+
+document.write(!(5 > 6))
+
+document.write(!(5 < 6))
